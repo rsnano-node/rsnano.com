6cd393ad7d72ac8d426bbdf1c2b2ebdf13024992
luxbe <luxbe@tutanota.com>
2023-09-12 15:13:17 +0200

    fix: update postcss config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,12 +1,9 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+const tailwindcssNesting = require('tailwindcss/nesting');
 
 const config = {
-	plugins: [
-		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
-		tailwindcss(), //Some plugins, like tailwindcss/nesting, need to run before Tailwind, tailwindcss(), //But others, like autoprefixer, need to run after, //But others, like autoprefixer, need to run after,
-		autoprefixer
-	]
+	plugins: [tailwindcssNesting(), tailwindcss(), autoprefixer()]
 };
 
 module.exports = config;
