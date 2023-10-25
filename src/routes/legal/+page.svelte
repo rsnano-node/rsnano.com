1d2ded0e98bf45346df3a6cfe12ca833ecbe5e70
luxbe <luxbe@tutanota.com>
2023-10-25 18:57:14 +0200

    feat: add legal route

---
+++ src/routes/legal/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import Section from '../Section.svelte';
+	import type { PageData } from './$types';
+</script>
+
+<svelte:head>
+	<title>RsNano | Legal</title>
+</svelte:head>
+
+<section class="hero text-center min-h-[16rem] bg-base-200">
+	<div class="hero-content">
+		<h1 class="text-4xl font-bold m-0">Legal Information</h1>
+	</div>
+</section>
+<main class="prose max-w-6xl mx-auto my-16">
+	<h2>Responsible for this website:</h2>
+	<div class="card bg-base-200">
+		<div class="card-body">
+			<span>Gustav Schauwecker</span>
+			<span>Gutenbergstr. 77a</span>
+			<span>c/o Coworking0711</span>
+			<span>70197 Stuttgart</span>
+			<span>Germany</span>
+		</div>
+	</div>
+
+	<h2>Contact:</h2>
+	<div class="card bg-base-200">
+		<div class="card-body">
+			<span>Telefon: +49 177 5338841</span>
+			<span>E-Mail: gustav@gustav-schauwecker.de</span>
+		</div>
+	</div>
+</main>
