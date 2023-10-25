996a40b09f385879036eab65daa9170160d57d96
luxbe <luxbe@tutanota.com>
2023-10-25 18:55:55 +0200

    feat: add ThemeToggle component

---
+++ src/lib/ThemeToggle/ThemeToggle.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { themeData } from '$lib/stores';
+	import { HalfMoonIcon, SunLightIcon } from '@indaco/svelte-iconoir';
+
+	const toggleTheme = () => {
+		themeData.update((theme) => {
+			return theme === 'dark' ? 'light' : 'dark';
+		});
+	};
+</script>
+
+<button class="btn btn-ghost btn-square" on:click={toggleTheme}>
+	{#if $themeData === 'dark'}
+		<SunLightIcon />
+	{:else}
+		<HalfMoonIcon />
+	{/if}
+</button>
