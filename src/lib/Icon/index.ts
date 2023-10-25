8d74dacfd07003d0eb80abeee23f93ba9d5fd190
luxbe <luxbe@tutanota.com>
2023-10-25 18:19:43 +0200

    feat: add Icon component

---
+++ src/lib/Icon/index.ts
@@ -0,0 +1,5 @@
+export { default as Logo } from './Logo.svelte';
+export { default as LogoText } from './LogoText.svelte';
+export { default as RustIcon } from './RustIcon.svelte';
+export { default as Representative } from './Representative.svelte';
+export { default as Donation } from './Donation.svelte';
