9c14d7f619ae3c4fdd8182d6cb2d38a96a4c923c
luxbe <luxbe@tutanota.com>
2023-10-25 19:20:28 +0200

    feat: allow arbitrary html in markdown blog posts

--- src/lib/utils/blog/parser.ts
+++ src/lib/utils/blog/parser.ts
@@ -1,13 +1,17 @@
-import { remarkFrontMatterYaml, remarkSummarizer } from './remarkPlugin';
+import rehypeRaw from 'rehype-raw';
 import rehypeStringify from 'rehype-stringify';
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import { unified } from 'unified';
+import { remarkFrontMatterYaml, remarkSummarizer } from './remarkPlugin';
 
 export const parser = unified()
 	.use(remarkParse)
-	.use(remarkRehype)
+	.use(remarkRehype, {
+		allowDangerousHtml: true
+	})
+	.use(rehypeRaw)
 	.use(rehypeStringify)
 	.use(remarkFrontmatter, ['yaml'])
 	.use(remarkFrontMatterYaml)
