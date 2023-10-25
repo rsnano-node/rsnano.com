ea30ad7dd711be1e5e8f20050ea81f3eaac5a8fd
luxbe <luxbe@tutanota.com>
2023-10-25 18:17:56 +0200

    feat: add card component

---
+++ src/lib/Card/Card.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let title: string;
+</script>
+
+<div class="card items-center text-center">
+	<slot name="icon" />
+	<h3 class="text-nano-blue font-bold text-3xl">{title}</h3>
+	<slot />
+</div>
